acf92b698ce02de43d0582d5f3a3a42bece3e2ae
Lucas <scorpionplaysb@gmail.com>
2026-04-07 15:30:45 -0300

    New point and line classes

--- myframe.cpp
+++ myframe.cpp
@@ -3,91 +3,31 @@
 #include <QPaintEvent>
 
 MyFrame::MyFrame(QWidget *parent) : QFrame(parent)
-{
-    linha = QLine(0, 0, 800, 620);
-    retangulo = QRect(50, 50, 200, 100);
-    flag = false;
-    flagRect = false;
-
-    setFocusPolicy(Qt::StrongFocus);
-    setFocus();
-}
+{}
 
 void MyFrame::paintEvent(QPaintEvent *event)
 {
     QFrame::paintEvent(event);
 
     QPainter painter(this);
-    painter.setPen(Qt::blue);
+    painter.setPen(Qt::black);
 
-    if(flag){
-        painter.drawLine(linha);
-        flag = false;
+    ponto.desenharPonto(&painter);
+    linha.desenharLinha(&painter);
+}
+
+MyPoint::MyPoint(int x, int y) : x(x), y(y){}
+
+void MyPoint::desenharPonto(QPainter *painter)
+{
+    for(int i = 0; i < 10; i++){
+        painter->drawPoint(x + i, y);
     }
-
-    if(flagRect){
-        painter.setPen(QPen(Qt::black, 3));
-        painter.setBrush(Qt::yellow);
-        painter.drawRect(retangulo);
-
-        flagRect = false;
-    }
 }
 
-void MyFrame::keyPressEvent(QKeyEvent *keyEvent)
+MyLine::MyLine(MyPoint i, MyPoint f) : i(i), f(f){}
+
+void MyLine::desenharLinha(QPainter *painter)
 {
-    switch(keyEvent->key()){
-        case Qt::Key_W:
-            retangulo = QRect(retangulo.x(), retangulo.y() - 10, retangulo.width(), retangulo.height());
-            break;
-        case Qt::Key_S:
-            retangulo = QRect(retangulo.x(), retangulo.y() + 10, retangulo.width(), retangulo.height());
-            break;
-        case Qt::Key_A:
-            retangulo = QRect(retangulo.x() - 10, retangulo.y(), retangulo.width(), retangulo.height());
-            break;
-        case Qt::Key_D:
-            retangulo = QRect(retangulo.x() + 10, retangulo.y(), retangulo.width(), retangulo.height());
-            break;
-    }
-
-    QRect area = contentsRect(); // contentsRect representa a área de desenho (no caso os limites do frame)
-
-    // Limites horizontais
-    if(retangulo.left() < area.left())
-        retangulo.moveLeft(area.left());
-
-    if(retangulo.right() > area.right())
-        retangulo.moveRight(area.right());
-
-    // Limites verticais
-    if(retangulo.top() < area.top())
-        retangulo.moveTop(area.top());
-
-    if(retangulo.bottom() > area.bottom())
-        retangulo.moveBottom(area.bottom());
-
-    flagRect = true;
-    update();
+    painter->drawLine(i, f);
 }
-
-void MyFrame::mostrar()
-{
-    flag = true;
-    update(); //update para atualizar o frame
-}
-
-void MyFrame::apagar()
-{
-    flag = false;
-    flagRect = false;
-    update();
-}
-
-void MyFrame::desenharRect()
-{
-    setFocus();
-    flagRect = true;
-    update();
-}
-
--- myframe.h
+++ myframe.h
@@ -1,8 +1,39 @@
 #ifndef MYFRAME_H
 #define MYFRAME_H
 
+#include <iostream>
+#include <ostream>
 #include <QFrame>
+#include <QPoint>
 #include <QWidget>
+
+class MyPoint : public QPoint{
+private:
+    int x, y;
+
+public:
+    MyPoint(int x, int y);
+
+    get_x(){
+        return x;
+    }
+
+    get_y(){
+        return y;
+    }
+
+    void desenharPonto(QPainter *painter);
+};
+
+class MyLine{
+private:
+    MyPoint i, f;
+
+public:
+    MyLine(MyPoint i, MyPoint f);
+    std::cout << i.get_x() << i.get_y();
+    void desenharLinha(QPainter *painter);
+};
 
 class MyFrame : public QFrame
 {
@@ -10,18 +41,10 @@ class MyFrame : public QFrame
 public:
     MyFrame(QWidget *parent = nullptr);
     void paintEvent(QPaintEvent *event) override;
-    void keyPressEvent(QKeyEvent *keyEvent) override;
-
-public slots:
-    void mostrar();
-    void apagar();
-    void desenharRect();
 
 private:
-    QLine linha;
-    QRect retangulo;
-    bool flag;
-    bool flagRect;
+    MyPoint ponto = MyPoint(100, 100);
+    MyLine linha = MyLine(MyPoint(100, 200), MyPoint(400, 200));
 };
 
 #endif // MYFRAME_H
